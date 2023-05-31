33e1d975eb6ee344ee31354f3849902f9d1798cf
Marco Kuidja <kuidjamarco@gmail.com>
2023-05-31 23:48:58 +0100

    added order filtering textfiled to orders page

--- apps/app/src/pages/orders.tsx
+++ apps/app/src/pages/orders.tsx
@@ -1,7 +1,18 @@
-import { IOrder } from '@e-pressing/interfaces';
-import { Box, Typography } from '@mui/material';
-import { useState, useEffect } from 'react';
+import { ClothStatus, IOrder } from '@e-pressing/interfaces';
+import { FilterList } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Collapse,
+  IconButton,
+  MenuItem,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
 import OrderCard from '../components/orderCard';
+import { theme } from '../theme';
 
 export interface IOrdersProps {
   children?: JSX.Element;
@@ -10,7 +21,12 @@ export interface IOrdersProps {
 export default function Orders(props: IOrdersProps) {
   const [orders, setOrders] = useState<IOrder[]>([]);
 
-  //TODO call api to fetch registered orders
+  const [expanded, setExpanded] = useState(false);
+
+  const [status, setStatus] = useState<ClothStatus>('PENDING');
+  const [orderNumber, setOrderNUmber] = useState<string>('');
+
+  //TODO call api to fetch registered orders with status and orderNumber
   useEffect(() => {
     setOrders([
       {
@@ -77,14 +93,81 @@ export default function Orders(props: IOrdersProps) {
         status: 'REMOVED',
       },
     ]);
-  }, []);
+  }, [status, orderNumber]);
 
   return (
-    <Box>
-      <Typography variant="h4">All recorded cloths</Typography>
-      {orders.map((order) => (
-        <OrderCard {...order} />
-      ))}
+    <Box sx={{ minWidth: '50vw', display: 'grid', gridAutoFlow: 'row' }}>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: '1fr auto auto',
+        }}
+      >
+        <Typography variant="h4">All recorded cloths</Typography>
+        <Collapse
+          timeout="auto"
+          unmountOnExit
+          in={expanded}
+          orientation="horizontal"
+        >
+          <Box
+            sx={{
+              display: 'grid',
+              gap: theme.spacing(1),
+              gridAutoFlow: 'column',
+            }}
+          >
+            <TextField
+              select
+              fullWidth
+              id="status"
+              name="status"
+              label="Order status"
+              size="small"
+              variant="standard"
+              value={status}
+              defaultValue={status}
+              onChange={(e) => setStatus(e.target.value as ClothStatus)}
+            >
+              <MenuItem value="PEDING">Pending</MenuItem>
+              <MenuItem value="READY">Ready</MenuItem>
+              <MenuItem value="REMOVED">Removed</MenuItem>
+            </TextField>
+            <TextField
+              id="order_number"
+              name="order_number"
+              label="Order number"
+              size="small"
+              variant="standard"
+              value={orderNumber}
+              onChange={(e) => setOrderNUmber(e.target.value)}
+            />
+          </Box>
+        </Collapse>
+        <Tooltip title="Filter list">
+          <IconButton onClick={() => setExpanded((show) => !show)}>
+            <FilterList />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      {orders.length === 0 ? (
+        <Box
+          sx={{
+            display: 'grid',
+            gridAutoFlow: 'row',
+            gap: theme.spacing(2),
+            justifySelf: 'center',
+            padding: theme.spacing(2),
+          }}
+        >
+          <Typography variant="h6">No order is registered yet !</Typography>
+          <Button variant="contained">
+            new order
+          </Button>
+        </Box>
+      ) : (
+        orders.map((order) => <OrderCard {...order} />)
+      )}
     </Box>
   );
 }
