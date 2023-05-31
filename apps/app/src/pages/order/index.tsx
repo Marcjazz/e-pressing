45120ad7637c3db783302aed7e9eb28078b8a27f
Marco Kuidja <kuidjamarco@gmail.com>
2023-05-31 01:15:08 +0100

    reorganized order page and used input card for refactoring

--- apps/app/src/pages/order/index.tsx
+++ apps/app/src/pages/order/index.tsx
@@ -1,7 +1,8 @@
 import { ICreateCloth } from '@e-pressing/interfaces';
 import { Add } from '@mui/icons-material';
-import { Box, IconButton, TextField, Tooltip, Typography } from '@mui/material';
+import { Box, IconButton, Tooltip, Typography } from '@mui/material';
 import { useState } from 'react';
+import InputCard from '../../components/inputCard';
 
 export interface IOrderProps {
   children?: JSX.Element;
@@ -11,75 +12,45 @@ export type Error =
   | { field: 'cloths'; fake_id: number; error: string };
 
 function useNewOrder() {
-  const [newOrder, setNewOrder] = useState<{
-    cloths: { fake_id: number; value: ICreateCloth }[];
-    description: string;
-  }>({
-    cloths: [],
-    description: '',
-  });
+  const [newItems, setNewItems] = useState<
+    { fake_id: number; value: ICreateCloth }[]
+  >([]);
 
   const [errors, setErrors] = useState<Error[]>();
 
-  const descriptionHandler = (description: string) => {
-    setNewOrder((newOrder) => ({ ...newOrder, description }));
-  };
-
   const addClothHandler = () => {
-    setNewOrder((newOrder) => ({
-      ...newOrder,
-      cloths: [
-        ...newOrder.cloths,
-        {
-          fake_id: newOrder.cloths.length + 1,
-          value: {
-            cloth_color: '',
-            cloth_name: '',
-            quantity: 1,
-            remarks: '',
-            washing_price: 100,
-          },
+    setNewItems((newItems) => [
+      ...newItems,
+      {
+        fake_id: newItems.length + 1,
+        value: {
+          cloth_color: '',
+          cloth_name: '',
+          quantity: 1,
+          remarks: '',
+          washing_price: 100,
         },
-      ],
-    }));
+      },
+    ]);
   };
 
   const removeClothHandler = (fake_id: number) => {
-    setNewOrder((newOrder) => ({
-      ...newOrder,
-      cloths: newOrder.cloths.filter((_) => _.fake_id === fake_id),
-    }));
+    setNewItems((newItems) => newItems.filter((_) => _.fake_id === fake_id));
   };
 
   const updateClothHandler = (
     fake_id: number,
     updatedCloth: Partial<ICreateCloth>
   ) => {
-    setNewOrder((newOrder) => ({
-      ...newOrder,
-      cloths: newOrder.cloths.map((cloth) =>
+    setNewItems((newItems) =>
+      newItems.map((cloth) =>
         cloth.fake_id === fake_id ? { ...cloth, ...updatedCloth } : cloth
-      ),
-    }));
+      )
+    );
   };
 
   const sumbitNewOrderHandler = () => {
-    const { cloths, description } = newOrder;
-    if (!description) {
-      const newError: Error = {
-        field: 'description',
-        error: 'Description is required !',
-      };
-      setErrors((errors) => (errors ? [...errors, newError] : [newError]));
-    }
-    if (description.length > 255) {
-      const newError: Error = {
-        field: 'description',
-        error: 'Description is too long. Please make it sort',
-      };
-      setErrors((errors) => (errors ? [...errors, newError] : [newError]));
-    }
-    if (cloths.length === 0) {
+    if (newItems.length === 0) {
       const newError: Error = {
         field: 'cloths',
         fake_id: 0,
@@ -87,7 +58,7 @@ function useNewOrder() {
       };
       setErrors((errors) => (errors ? [...errors, newError] : [newError]));
     } else {
-      cloths.forEach(({ fake_id, value: cloth }) => {
+      newItems.forEach(({ fake_id, value: cloth }) => {
         Object.keys(cloth).forEach((key) =>
           !cloth[key as keyof ICreateCloth]
             ? setErrors((errors) =>
@@ -106,29 +77,27 @@ function useNewOrder() {
       });
     }
 
-    if (errors?.length === 0)
-      return { description, cloths: cloths.map((_) => _.value) };
+    if (errors?.length === 0) return newItems.map((_) => _.value);
   };
 
   return {
-    newOrder,
+    newItems,
     dispatchers: {
       addClothHandler,
       removeClothHandler,
       updateClothHandler,
       sumbitNewOrderHandler,
-      descriptionHandler,
     },
   };
 }
-export default function Order({ children = <>Hello world</> }: IOrderProps) {
+export default function Order(props: IOrderProps) {
   const {
-    newOrder: { cloths, description },
-    dispatchers: { descriptionHandler, addClothHandler, updateClothHandler },
+    newItems,
+    dispatchers: { addClothHandler, updateClothHandler },
   } = useNewOrder();
   return (
-    <Box>
-      <Typography variant="h3">Enregistrer une commande</Typography>
+    <Box height={'100%'}>
+      <Typography variant="h3">New record</Typography>
       <Box
         sx={{
           display: 'grid',
@@ -141,91 +110,26 @@ export default function Order({ children = <>Hello world</> }: IOrderProps) {
             display: 'grid',
             gridTemplateColumns: '1fr auto',
             columnGap: 2,
+            alignItems: 'center',
           }}
         >
-          <TextField
-            id="description"
-            name="description"
-            label="Description"
-            size="small"
-            variant="outlined"
-            fullWidth
-            value={description}
-            onChange={(e) => descriptionHandler(e.target.value)}
-          />
+          <Typography variant="subtitle2">
+            {new Date().toUTCString()}
+          </Typography>
           <IconButton onClick={addClothHandler}>
             <Tooltip title="add new Cloth">
               <Add color="secondary" />
             </Tooltip>
           </IconButton>
         </Box>
-        {cloths.map(
-          ({
-            fake_id,
-            value: { cloth_color, cloth_name, quantity, washing_price },
-          }) => (
-            <Box
-              key={fake_id}
-              sx={{ display: 'grid', gridAutoFlow: 'column', columnGap: 2 }}
-            >
-              <TextField
-                id={`cloth_name-${fake_id}`}
-                name={`cloth_name-${fake_id}`}
-                label="Cloth Description"
-                size="small"
-                variant="outlined"
-                value={cloth_name}
-                sx={{ width: '50vw' }}
-                onChange={(e) => updateClothHandler(fake_id, { cloth_name })}
-              />
-              <Box
-                sx={{ display: 'grid', gridAutoFlow: 'column', columnGap: 1 }}
-              >
-                <TextField
-                  id={`quantity-${fake_id}`}
-                  name={`quantity-${fake_id}`}
-                  label="Quantity"
-                  size="small"
-                  variant="outlined"
-                  type="number"
-                  value={quantity}
-                  onChange={(e) => updateClothHandler(fake_id, { quantity })}
-                />
-                <TextField
-                  id={`cloth_color-${fake_id}`}
-                  name={`cloth_color-${fake_id}`}
-                  label="Cloth color"
-                  size="small"
-                  variant="outlined"
-                  value={cloth_color}
-                  onChange={(e) => updateClothHandler(fake_id, { cloth_color })}
-                />
-                <TextField
-                  id={`washing_price-${fake_id}`}
-                  name={`washing_price-${fake_id}`}
-                  label="Washing price"
-                  size="small"
-                  variant="outlined"
-                  type="number"
-                  value={washing_price}
-                  onChange={(e) =>
-                    updateClothHandler(fake_id, { washing_price })
-                  }
-                />
-                <TextField
-                  id={`partial_total-${fake_id}`}
-                  name={`partial_total-${fake_id}`}
-                  label="Partial total"
-                  size="small"
-                  variant="outlined"
-                  type="number"
-                  disabled
-                  value={quantity * washing_price}
-                />
-              </Box>
-            </Box>
-          )
-        )}
+        {newItems.map(({ fake_id, value }) => (
+          <InputCard
+            {...value}
+            key={fake_id}
+            textFieldId={fake_id}
+            onChangeHandler={updateClothHandler}
+          />
+        ))}
       </Box>
     </Box>
   );
